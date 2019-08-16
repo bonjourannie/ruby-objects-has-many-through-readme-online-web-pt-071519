62fcf86bbf92f2dc0aaf602c8584382a3cc99f51
Annie Truex <artruex@gmail.com>
2019-08-16 23:47:42 +0000

    Automatically backed up by Learn

--- lib/customer.rb
+++ lib/customer.rb
@@ -1,3 +1,17 @@
 class Customer
   
+  attr_accessor :name, :age 
+  
+  @@all = []
+  
+  def initialize(name, age)
+    @age = age 
+    @name = name
+    @@all << self 
+  end
+  
+  def self.all 
+    @@all 
+  end
+  
 end
